5e179dabfc8334f95cd879c73e0b2bc04ae9f13e
jhonRawls <jhonRawls@gmail.com>
2018-01-20 19:32:55 +0800

    commit

--- springCloud/eureka/src/main/java/cn/ibadi/eureka/EurekaApplication.java
+++ springCloud/eureka/src/main/java/cn/ibadi/eureka/EurekaApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+/**
+ * @author jhon
+ * */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
-		System.out.print("eureka注册中心启动......");
+		System.out.print("eureka注册中心启动..........");
 	}
 }
